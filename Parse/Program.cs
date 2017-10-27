35202a71572fdc8f27e21aa3bf937890abfeb984
Alex <skripinalexey1987@gmail.com>
2017-10-27 08:52:51 +0500

    Добавьте файлы проекта.

---
+++ Parse/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlQueryCreator sq = new SqlQueryCreator();
+            sq.PathErrorLog = "D:\\ErrorLog.csv";
+            sq.PathSnilsLog = "D:\\SnilsLog.csv";
+            var res = sq.CreateQuerySQL();
+        }
+    }
+}
---
+++ Parse/SqlQueryCreator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Parse
+{
+    class SqlQueryCreator
+    {
+        public string PathErrorLog { get; set; }
+
+        public string PathSnilsLog { get; set; }
+
+        private ICollection<string> GetErrorsSNILS()
+        {
+            ICollection<string> result = new List<string>();
+            ICollection<string> linesErr = new List<string>();
+            ICollection<string> lineSnl = new List<string>();
+            linesErr = File.ReadAllLines(PathErrorLog);
+            lineSnl = File.ReadAllLines(PathSnilsLog);
+            foreach (var s in linesErr)
+            {
+                List<string> list = new List<string>();
+                list = s.Split(';').Where(ss => ss.Equals(ss)).ToList();
+                foreach(var sl in lineSnl)
+                {
+                    List<string> listn = new List<string>();
+                    listn = sl.Split(';').Where(q => q.Equals(q)).ToList();
+                    if (listn.Contains(list.FirstOrDefault()))
+                    {
+                        result.Add(listn.ElementAt(1));
+                        if(listn.ElementAt(1).Equals(listn.ElementAt(2)))
+                        {
+                            result.Add(listn.ElementAt(2));
+                        }
+                    }
+                }
+            }
+            return result.Distinct().ToList();
+        }
+
+        public string CreateQuerySQL()
+        {
+            List<string> list = new List<string>();
+            list = GetErrorsSNILS().ToList();
+            string result = "SELECT NPS FROM F2 WHERE NPS = '";
+            foreach(var r in list)
+            {
+                result += r + "' OR NPS = '";
+            }
+            return result;
+        }
+    }
+}
